5985a6e85c7dc82a034ed12a877873a76e71245a
amir8602 <amirreza8602@gmail.com>
2022-06-27 21:50:16 +0430

    palindrome

---
+++ src/palindrome/Main.java
@@ -0,0 +1,28 @@
+package palindrome;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(checkPalindrome(input));
+        scanner.close();
+
+    }
+
+    public static boolean checkPalindrome(String input){
+        Boolean check = true;
+        char[] characters = input.toCharArray();
+        for (int i = 0; i < characters.length/2; i++) {
+            if(characters[i]!=characters[characters.length-1-i]){
+                check =false;
+                break;
+            }
+        }
+        if (check)
+            return true;
+        else
+            return false;
+    }
+}
