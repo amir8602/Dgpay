ff9ffd715bd8bd55b67d6be5e22ea150d025a13d
amir8602 <amirreza8602@gmail.com>
2022-07-04 18:40:23 +0430

    i dont know

--- src/first/Main.java
+++ src/first/Main.java
@@ -1,33 +1,77 @@
 package first;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
+//        String goodString = new String("aaabbbb");
+//        char[] stringToChar = goodString.toCharArray();
+//        // String
+//        Integer count = 1;
+//        ArrayList<Character> typeOfChars = new ArrayList<Character>();
+//        ArrayList<Integer> countOfChars = new ArrayList<Integer>();
+//
+//        for (int i = 0; i < stringToChar.length - 1; i++) {
+//            if (stringToChar[i] == ' ') {
+//                continue;
+//            }
+//            typeOfChars.add(stringToChar[i]);
+//            for (int j = i + 1; j < stringToChar.length; j++) {
+//                if (stringToChar[i] == stringToChar[j]) {
+//                    count++;
+//                    stringToChar[j] = ' ';
+//                }
+//            }
+//            countOfChars.add(count);
+//            count = 1;
+//
+//        }
+//        System.out.println(countOfChars);
+//        System.out.println(countOfChars.size());
+//        System.out.println(Collections.max(countOfChars));
+//
+//
+//        Map<String, Integer> solve = new HashMap<String, Integer>();
+//        // for (int i = 0; i < ; i++) {
+//
+//    }
 
-        String str = new String("akram badiri");
-        char [] stringToChar = str.toCharArray();
 
-        Integer count = 1;
-        ArrayList<Character> chars = new ArrayList<Character>();
-        ArrayList<Integer> countOfChars = new ArrayList<Integer>();
+//    List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+//    long count = strList.stream()
+//            .filter(String::isEmpty)
+//            .count();
+//
+//
+//        List<String> collect = strList.stream().filter(y -> y.startsWith("a")).collect(Collectors.toList());
+//        System.out.println(collect.get(0));
 
-        for (int i = 0; i <stringToChar.length-1 ; i++) {
-            if(stringToChar[i] == ' '){
-                continue;
-            }
-            chars.add(stringToChar[i]);
-            for (int j = i+1; j <stringToChar.length ; j++) {
-                if(stringToChar[i]==stringToChar[j]){
-                    count++;
-                    stringToChar[j]=' ';
-                }
-            }
-            countOfChars.add(count);
-            count=1;
 
-        }
-        System.out.println(countOfChars);
+//        List<String> g7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
+//
+//
+//
+//        String collect1 = g7.stream().map(String::toUpperCase).collect(Collectors.joining(", "));
+//        System.out.println(collect1);
+//
+//
+//
+//        String concat1="";
+//        for (int i = 0; i < g7.size(); i++) {
+//            concat1 = concat1.concat(g7.get(i))+", ";
+//        }
+//        System.out.println(concat1);
+
+
+
+//        List<Integer> primes = Arrays.asList(2,3,5,7,11,17,19,23,29);
+//        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+//        System.out.println(stats);
+
+
+
+
     }
 }
